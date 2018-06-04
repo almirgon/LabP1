0ec7c959ce607a3529402d7aa46a94dcc0d9aeda
Almir Crispiniano <37631373+almirgon@users.noreply.github.com>
2018-06-04 14:29:59 -0300

    Update mdc.py

--- Unidade-5/mdc.py
+++ Unidade-5/mdc.py
@@ -5,9 +5,34 @@
 D = int(raw_input())
 d = int(raw_input())
 
+maior = 0
+menor = 0
 resto = 0
-if D > d:
-    aux = D
-    while aux >= d:
-        aux -= d
-	resto = aux 
+if (D == d and d != 0) or d == 0:
+	print D
+elif D == 0:
+	print d
+	
+elif D > d:
+    resto = D
+    maior = D
+    menor = d
+    while resto >= d:
+        resto -= d
+    maior = menor
+    menor = resto
+    while menor< maior:
+		maior -= menor
+
+elif d > D:
+    resto = d
+    maior = d
+    menor = D
+    while resto >= D:
+        resto -= D
+    maior = menor
+    menor = resto
+    while menor < maior:
+		maior -= menor
+if D != d and D != 0 and d != 0:
+	print maior
